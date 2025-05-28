1a8d07023f5de248003d6f7c5bd6a93d3d0e387c
Monir <monir.nub.cse996@gmail.com>
2025-05-28 18:34:52 +0600

    initial

--- lib/main.dart
+++ lib/main.dart
@@ -10,21 +10,67 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Basics',
+
+      title: 'Flutter Notebook',
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+
+      //Light Theme
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.deepOrange,
+            brightness: Brightness.light
+        ),
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Theme.of(context).colorScheme.surface,
+          foregroundColor: Theme.of(context).colorScheme.onSurface,
+          elevation: 10,
+        )
       ),
-      home: const HomeScreen(),
+
+
+      //Dark Theme
+      darkTheme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: Colors.amber,
+              brightness: Brightness.dark
+          ),
+          appBarTheme: AppBarTheme(
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            foregroundColor: Theme.of(context).colorScheme.onPrimary,
+            elevation: 10,
+          )
+      ),
+
+      home: HomeScreen(),
     );
   }
 }
+
+
+
+
+
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+
+      appBar: AppBar(
+        title: Text("Material App"),
+        //backgroundColor: Colors.red
+      ),
+
+      body: Center(child: Text("Home Screen")),
+
+
+    );
   }
 }
 

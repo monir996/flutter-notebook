f9b8b3381bdcae1f654a94367008be1eba707fee
Monir <monir.nub.cse996@gmail.com>
2025-05-28 23:45:32 +0600

    Container Widget initial

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Basics',
+      title: 'Flutter Notebook',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
@@ -24,7 +25,27 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+
+      appBar: AppBar(title: Text("Container Widget"), backgroundColor: Colors.amber,),
+      backgroundColor: Colors.blue,
+
+      // ----------------------- Branch 3 ==> Container Widget ------------------
+      body: Container(
+        width: 200,
+        height: 200,
+        //color: Colors.yellowAccent, //use background color only here OR only inside the BoxDecoration
+        margin: EdgeInsets.all(30),
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+            color: Colors.white70,
+            //shape: BoxShape.circle
+            borderRadius: BorderRadius.circular(16)
+        ),
+        transform: Matrix4.rotationZ(0.1),
+        child: Text("Hello Flutter"),
+      ),
+    );
   }
 }
 

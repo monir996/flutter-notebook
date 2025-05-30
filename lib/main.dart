75556a359785d444b09c2fa4f854c5392bf9643c
Monir <monir.nub.cse996@gmail.com>
2025-05-30 22:06:30 +0600

    Image Widget - initial

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
-// ----------------------- Main Branch  ==> Basic Structure ------------------
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -26,7 +26,29 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Flutter Notebook"), backgroundColor: Colors.lightGreenAccent),
+      appBar: AppBar(title: Text("Image Widget"), backgroundColor: Colors.lightGreenAccent),
+
+      // ----------------------- Branch 9 ==> Image Widget ------------------
+      body: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(20),
+              child: Image.asset("assets/images/nature.jpg"), //Offline Image
+            ),
+
+            SizedBox(height: 30),
+            
+            Container(
+              padding: EdgeInsets.all(20),
+              child: Image.network(
+                  "https://images.pexels.com/photos/1477166/pexels-photo-1477166.jpeg", //From Internet
+                  height: 220,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+              ),
+            )
+          ] 
+      ),
     );
   }
 }

97e296bf9395b6b38b893e02c205dac7a2a4d152
Monir <monir.nub.cse996@gmail.com>
2025-05-30 21:10:59 +0600

    Text Widget - initial

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
-// ----------------------- Main Branch  ==> Basic Structure ------------------
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -26,7 +26,45 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Flutter Notebook"), backgroundColor: Colors.lightGreenAccent),
+      appBar: AppBar(title: Text("Text Widget"), backgroundColor: Colors.redAccent),
+
+
+      // ----------------------- Branch 8 ==> Text Widget ------------------
+      body: Center(
+        child: Column(
+          children: [
+
+
+            // ----------------------- Normal Text Widget ------------------
+            Text(
+              "Normal Text Widget",
+              style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold
+              ),
+              textAlign: TextAlign.center,
+              //overflow: TextOverflow.ellipsis,
+              //maxLines: 2,
+            ),
+
+            SizedBox(height: 40),
+
+            // ----------------------- Rich Text Widget ------------------
+            RichText(
+                text: TextSpan(
+                    children: [
+                      TextSpan(text: "Rich ", style: TextStyle(color: Colors.red, fontSize: 24, fontWeight: FontWeight.bold)),
+                      TextSpan(text: "Text ", style: TextStyle(color: Colors.green, fontSize: 28, fontWeight: FontWeight.bold, fontStyle: FontStyle.italic)),
+                      TextSpan(text: "Widget", style: TextStyle(color: Colors.red, fontSize: 24, fontWeight: FontWeight.bold, fontStyle: FontStyle.italic)),
+                    ]
+                )
+            )
+
+
+          ],
+        ),
+      ),
     );
   }
 }

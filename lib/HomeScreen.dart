062a0b617a03ccf53d3f9df4a6a6c7d5bd030df3
Monir <monir.nub.cse996@gmail.com>
2025-05-31 12:19:46 +0600

    SnackBar - initial

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -8,12 +8,31 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  // ----------------------- Create SnackBar ------------------
+  mySnackBar(message, context){
+    return ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+            content: Text(message),
+            action: SnackBarAction(label: "Undo", onPressed: (){})
+        )
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Flutter Notebook", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
+      appBar: AppBar(title: Text("SnackBar Widget", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
 
-      // ----------------------- Main Branch  ==> Basic Structure ------------------
+      // ----------------------- Branch 15 ==> SnackBar Widget ------------------
+      body: Center(
+        child: TextButton(
+            onPressed: (){
+              mySnackBar("Delete Successful", context);
+            },
+            child: Text("Show SnackBar")
+        ),
+      ),
 
     );
   }

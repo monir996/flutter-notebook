6d681a3c060e28445a49b64ee4e15782fd6a570f
Monir <monir.nub.cse996@gmail.com>
2025-05-31 23:45:36 +0600

    Toast - initial

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,4 +1,8 @@
+import 'package:delightful_toast/delight_toast.dart';
+import 'package:delightful_toast/toast/components/toast_card.dart';
+import 'package:delightful_toast/toast/utils/enums.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({super.key});
@@ -11,11 +15,53 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Flutter Notebook", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
+      appBar: AppBar(title: Text("Toast Widget", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
 
-      // ----------------------- Main Branch  ==> Basic Structure ------------------
+      // ----------------------- Branch 17 ==> Toast Widget ------------------
 
-      body: Center(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            // ----------------------- Stylish Toast ------------------
+
+            TextButton(
+                onPressed: (){
+
+                  DelightToastBar(
+                    builder: (context) {
+                      return const ToastCard(
+                          leading: Icon(Icons.notifications_active, size: 32),
+                          title: Text("Toast Message", style: TextStyle(fontWeight: FontWeight.w700, fontSize: 14))
+                      );
+                    },
+                    position: DelightSnackbarPosition.top,
+                    autoDismiss: true,
+                    snackbarDuration: Duration(milliseconds: 2000)
+                  ).show(context);
+
+                },
+                child: Text("Stylish Toast")
+            ),
+
+            SizedBox(height: 20),
+
+            // ----------------------- Normal Toast ------------------
+
+            TextButton(
+                onPressed: (){
+                  Fluttertoast.showToast(
+                      msg: "Toast Message",
+                      gravity: ToastGravity.BOTTOM,
+                      backgroundColor: Color.fromRGBO(0, 0, 0, 0.5)
+                  );
+                },
+                child: Text("Normal Toast")
+            ),
+          ],
+        ),
+      ),
     );
   }
 }

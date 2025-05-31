c0cff4771afb9971e030ce00b6384e7741412863
Monir <monir.nub.cse996@gmail.com>
2025-05-31 11:53:42 +0600

    BottomNavigationBar - initial

--- lib/main.dart
+++ lib/main.dart
@@ -20,15 +20,47 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  // ----------------------- Current Index for BottomNavigationBar Item ------------------
+  int _selectedIndex = 0;
+
+  final List<Widget> _views = [
+    const Center(child: Text("Home", style: TextStyle(fontSize: 24, color: Colors.blue))),
+    const Center(child: Text("Profile", style: TextStyle(fontSize: 24, color: Colors.red))),
+    const Center(child: Text("Settings", style: TextStyle(fontSize: 24, color: Colors.pink))),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Flutter Notebook"), backgroundColor: Colors.lightGreenAccent),
+      appBar: AppBar(title: Text("Bottom Navigation Bar"), backgroundColor: Colors.lightGreenAccent),
 
-      // ----------------------- Main Branch  ==> Basic Structure ------------------
+      // ----------------------- Branch 14 ==> BottomNavigationBar Widget ------------------
+      body: SafeArea(
+          child: _views.elementAt(_selectedIndex)
+      ),
+
+      bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.person_2_outlined), label: "Profile"),
+            BottomNavigationBarItem(icon: Icon(Icons.settings), label: "Settings"),
+          ],
+          currentIndex: _selectedIndex,
+          onTap: (index){
+            setState(() {
+              _selectedIndex = index;
+            });
+          },
+      ),
 
     );
   }

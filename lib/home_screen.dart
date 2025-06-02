d20fa99b289fff32323b9d2ea905d8d31ef20a18
Monir <monir.nub.cse996@gmail.com>
2025-06-02 19:06:26 +0600

    animated text - initial

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,14 +9,50 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  // ----------------------- Styles for ColorizeAnimatedText ------------------
+  static const colorizeColors = [
+    Colors.purple,
+    Colors.blue,
+    Colors.yellow,
+    Colors.red,
+  ];
+
+  static const colorizeTextStyle = TextStyle(
+    fontSize: 50.0,
+    color: Colors.black54,
+    fontWeight: FontWeight.bold,
+  );
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-      appBar: AppBar(title: Text("Flutter Notebook", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
+      appBar: AppBar(title: Text("Animated Text Widget", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
 
-      // ----------------------- Main Branch  ==> Basic Structure ------------------
+      // ----------------------- Branch 23  ==> Animated Text Widget ------------------
+      body: SafeArea(
+        child: Center(
 
-      body: Center(),
+          child: AnimatedTextKit(
+            animatedTexts: [
+              ColorizeAnimatedText("Coding Bloom", textStyle: colorizeTextStyle, colors: colorizeColors),
+            ],
+            isRepeatingAnimation: true,
+            repeatForever: true,
+            onTap: (){},
+          ),
+
+          // ------ For more animation style, see the Documentation of "AnimatedTextKit"  -------
+
+        ),
+      )
     );
   }
+
+
+
+
+
+
 }

9dfb7c7e0b4858599eefa56d6bb501554b3d47cf
Monir <monir.nub.cse996@gmail.com>
2025-06-04 01:23:47 +0600

    dropDownButton - initial

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -8,14 +8,50 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  String? selectedValue;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Flutter Notebook", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
+      appBar: AppBar(title: Text("DropDownButton Widget", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
 
-      // ----------------------- Main Branch  ==> Basic Structure ------------------
+      // ----------------------- Branch 28 ==> DropDownButton Widget ------------------
 
-      body: Center(),
+      body: Center(
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(color: Colors.grey, width: 1, style: BorderStyle.solid),
+            borderRadius: BorderRadius.circular(5)
+          ),
+          child: Padding(
+            padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+
+            child: DropdownButton(
+
+                elevation: 16,
+                hint: Text("File Action", style: TextStyle(fontSize: 16.0, color: Colors.black87)),
+                style: TextStyle(fontSize: 16.0, color: Colors.blue),
+                icon: Icon(Icons.arrow_drop_down_sharp),
+                value: selectedValue,
+                dropdownColor: Colors.black87,
+                underline: Container(),
+                items: [
+                  DropdownMenuItem(value: "Save",child: Text("Save")),
+                  DropdownMenuItem(value: "Open",child: Text("Open")),
+                  DropdownMenuItem(value: "Delete",child: Text("Delete", style: TextStyle(color: Colors.red))),
+                ],
+                onChanged: (String? newValue){
+                  setState(() {
+                    selectedValue = newValue;
+                  });
+                }
+
+            ),
+          ),
+        ),
+      ),
     );
   }
 }

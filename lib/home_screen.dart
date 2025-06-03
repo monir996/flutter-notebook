19e3544d1f8fa412855471516e73d7eab332ccb8
Monir <monir.nub.cse996@gmail.com>
2025-06-03 18:35:47 +0600

    auto-size-text - initial

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,11 +12,45 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Flutter Notebook", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
+      appBar: AppBar(title: Text("AutoSize Text Widget", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
 
-      // ----------------------- Main Branch  ==> Basic Structure ------------------
+      // ----------------------- Branch 25 ==> AutoSize Text Widget ------------------
 
-      body: Center(),
+      // AutoSizeText is used to automatically adjust the font size of the text
+      // so that it fits within its container without overflowing.
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            // ----------------------- Normal Text Widget ------------------
+            Container(
+              height: 200,
+              width: 250,
+              color: Colors.orange,
+              child: Text(
+                "AutoSizeText is used to automatically adjust the font size of the text, so that it fits within its container without overflowing.",
+                style: TextStyle(fontSize: 30),
+              ),
+            ),
+
+            SizedBox(height: 50),
+
+            // ----------------------- AutoSize Text Widget ------------------
+            Container(
+              height: 100,
+              width: 250,
+              color: Colors.greenAccent,
+              child: AutoSizeText(
+                "AutoSizeText is used to automatically adjust the font size of the text, so that it fits within its container without overflowing.",
+                style: TextStyle(fontSize: 30),
+                //maxLines: 5,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }

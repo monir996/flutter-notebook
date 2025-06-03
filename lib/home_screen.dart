b58f24e702e65da323cdb30bc9f6ea9a400388e7
Monir <monir.nub.cse996@gmail.com>
2025-06-03 18:17:48 +0600

    fitted-box - initial

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -11,11 +11,26 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Flutter Notebook", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
+      appBar: AppBar(title: Text("FittedBox Widget", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
 
-      // ----------------------- Main Branch  ==> Basic Structure ------------------
+      // ----------------------- Branch 24  ==> FittedBox Widget ------------------
 
-      body: Center(),
+      // FittedBox is used to scale and fit its child within the available space.
+      // It helps to prevent overflow and keeps the child widget responsive.
+
+      body: Center(
+        child: Container(
+          height: 200,
+          width: 300,
+          color: Colors.blueAccent,
+          padding: EdgeInsets.all(12),
+
+          child: FittedBox(
+              child: Text("Coding Bloom", style: TextStyle(color: Colors.white, fontSize: 200, fontWeight: FontWeight.bold))
+          ),
+
+        ),
+      ),
     );
   }
 }

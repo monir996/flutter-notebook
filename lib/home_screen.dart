b8f0ade482d8780eb65ff14e1e8bda22e33a6820
Monir <monir.nub.cse996@gmail.com>
2025-06-03 23:47:49 +0600

    stack - initial

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -11,11 +11,33 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Flutter Notebook", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
+      appBar: AppBar(title: Text("Stack Widget", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
 
-      // ----------------------- Main Branch  ==> Basic Structure ------------------
+      // ----------------------- Branch 27 ==> Stack Widget ------------------
 
-      body: Center(),
+      body: SafeArea(
+          child: Center(
+
+            // Stack is used to place widgets on top of each other.
+            // It helps to create overlapping or layered UI elements.
+
+          child: Stack(
+              alignment: Alignment.center,
+              clipBehavior: Clip.none,
+              children: [
+                Container(height: 300, width: 300, color: Colors.red),
+
+                Container(height: 200, width: 200, color: Colors.green),
+
+                Positioned(
+                    bottom: -50,
+                    left: 0,
+                    child: Container(height: 100, width: 100, color: Colors.yellowAccent)
+                ),
+              ],
+            ),
+          )
+      ),
     );
   }
 }

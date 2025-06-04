b9381cefe2b5d6fd9bf12b2ca909c89cd8124ac9
Monir <monir.nub.cse996@gmail.com>
2025-06-05 01:13:29 +0600

    slider - initial

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -8,14 +8,31 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  double _currentSliderValue = 10;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Flutter Notebook", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
+      appBar: AppBar(title: Text("Slider Widget", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
 
-      // ----------------------- Main Branch  ==> Basic Structure ------------------
+      // ----------------------- Branch 36 ==> Slider Widget ------------------
 
-      body: Center(),
+      body: Center(
+        child: Slider(
+            min: 0,
+            max: 100,
+            divisions: 100, // it will divide 100 parts, like: 1, 2, 3...100 /// if value = 20, then 40, 60, 80, 100
+            value: _currentSliderValue,
+            label: _currentSliderValue.round().toString(),
+            onChanged: (double value) {
+              setState(() {
+                _currentSliderValue = value;
+              });
+            },
+
+        ),
+      ),
     );
   }
 }

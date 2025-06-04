ddd46c55fcfe55b50a4909161d58e33a357e9950
Monir <monir.nub.cse996@gmail.com>
2025-06-04 18:50:26 +0600

    radio button - initial

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -7,15 +7,68 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
+enum Lang {kotlin, dart, java}
+
 class _HomeScreenState extends State<HomeScreen> {
+
+  Lang? _selectedLang = Lang.dart;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Flutter Notebook", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
+      appBar: AppBar(title: Text("RadioButton Widget", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
 
-      // ----------------------- Main Branch  ==> Basic Structure ------------------
+      // ----------------------- Branch 30 ==> Radio Button Widget ------------------
 
-      body: Center(),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 50),
+            Text("Which Language do you like?"),
+            SizedBox(height: 50),
+
+            ListTile(
+              title: Text("Kotlin"),
+              leading: Radio(
+                  value: Lang.kotlin,
+                  groupValue: _selectedLang,
+                  onChanged: (value) {
+                    setState(() {
+                      _selectedLang = value;
+                    });
+                  }
+              ),
+            ),
+
+            ListTile(
+              title: Text("Dart"),
+              leading: Radio(
+                  value: Lang.dart,
+                  groupValue: _selectedLang,
+                  onChanged: (value) {
+                    setState(() {
+                      _selectedLang = value;
+                    });
+                  }
+              ),
+            ),
+
+            ListTile(
+              title: Text("Java"),
+              leading: Radio(
+                  value: Lang.java,
+                  groupValue: _selectedLang,
+                  onChanged: (value) {
+                    setState(() {
+                      _selectedLang = value;
+                    });
+                  }
+              ),
+            ),
+
+          ],
+        ),
+      ),
     );
   }
 }

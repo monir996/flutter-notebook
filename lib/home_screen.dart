4f82ae9fcb4462febae104dfa3ba3771405f2bf5
Monir <monir.nub.cse996@gmail.com>
2025-06-04 23:20:37 +0600

    font-awesome - initial

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({super.key});
@@ -11,11 +12,18 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Flutter Notebook", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
+      appBar: AppBar(title: Text("Font Awesome Icon", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
 
-      // ----------------------- Main Branch  ==> Basic Structure ------------------
+      // ----------------------- Branch 33 ==> Font Awesome Icon ------------------
 
-      body: Center(),
+      body: Center(
+        child: IconButton(
+            onPressed: (){}, 
+            icon: FaIcon(FontAwesomeIcons.mugHot),
+            color: Colors.red,
+            iconSize: 40.0,
+        ),
+      ),
     );
   }
 }

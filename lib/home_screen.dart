4a48dca2b23c4acb50da3464ea5a80fef6ebef33
Monir <monir.nub.cse996@gmail.com>
2025-06-04 20:23:09 +0600

    checkbox - initial

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -8,14 +8,72 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  bool? sun = false;
+  bool? mon = false;
+  bool? tue = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Flutter Notebook", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
+      appBar: AppBar(title: Text("CheckBox Widget", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
 
-      // ----------------------- Main Branch  ==> Basic Structure ------------------
+      // ----------------------- Branch 31 ==> CheckBox Widget ------------------
 
-      body: Center(),
+      body: Center(
+        child: Column(
+          children: [
+            CheckboxListTile(
+                value: sun,
+                onChanged: (value){
+                  setState(() {
+                    sun = value;
+                  });
+                },
+              activeColor: Colors.deepPurple,
+              tileColor: Colors.black12,
+              title: Text("Sunday", style: TextStyle(fontSize: 18)),
+              subtitle: Text("Day1"),
+              controlAffinity: ListTileControlAffinity.leading,
+              //tristate: true,
+            ),
+
+            SizedBox(height: 10),
+
+            CheckboxListTile(
+              value: mon,
+              onChanged: (value){
+                setState(() {
+                  mon = value;
+                });
+              },
+              activeColor: Colors.deepPurple,
+              tileColor: Colors.black12,
+              title: Text("Monday", style: TextStyle(fontSize: 18)),
+              subtitle: Text("Day2"),
+              //controlAffinity: ListTileControlAffinity.leading,
+              //tristate: true,
+            ),
+
+            SizedBox(height: 10),
+
+            CheckboxListTile(
+              value: tue,
+              onChanged: (value){
+                setState(() {
+                  tue = value;
+                });
+              },
+              activeColor: Colors.deepPurple,
+              tileColor: Colors.black12,
+              title: Text("Tuesday", style: TextStyle(fontSize: 18)),
+              subtitle: Text("Day3"),
+              //controlAffinity: ListTileControlAffinity.leading,
+              //tristate: true,
+            ),
+          ],
+        )
+      ),
     );
   }
 }

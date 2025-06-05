b6622e8609a13508ff1398d1ce4658d9ee36ad20
Monir <monir.nub.cse996@gmail.com>
2025-06-06 04:18:17 +0600

    aspect ratio - initial

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -13,11 +13,27 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
 
     return Scaffold(
-      appBar: AppBar(title: Text("Flutter Notebook", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
+      appBar: AppBar(title: Text("Aspect Ratio Widget", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
 
-      // ----------------------- Main Branch  ==> Basic Structure ------------------
+      // ----------------------- Branch 51 ==> Aspect Ratio Widget ------------------
 
-      body: Center(),
+      // AspectRatio is used to maintain a specific width-to-height ratio
+      // for a widget, which helps make the UI look consistent and responsive on different screen sizes.
+
+      /*
+      *   16 / 9   ==>	Video players, YouTube, modern widescreen displays
+      *   4 / 3    ==>	Photography, older TV screens, classic monitors
+      *   1        ==>	Square images, profile pictures, icons
+      *   3 / 2    ==>	Medium aspect ratio, cards, general-purpose images
+      *
+      * */
+
+      body: SafeArea(
+          child: AspectRatio(
+              aspectRatio: 3/2,
+              child: Image.network("https://images.pexels.com/photos/57416/cat-sweet-kitty-animals-57416.jpeg")
+          )
+      ),
     );
   }
 }

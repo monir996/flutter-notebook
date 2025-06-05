816c32a3d8545480d0715df9f14a1af7cadedb2e
Monir <monir.nub.cse996@gmail.com>
2025-06-05 13:47:14 +0600

    cupertino action sheet - initial

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,14 +9,43 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  
   @override
   Widget build(BuildContext context) {
+
+    // ----------------------- CupertinoActionSheet Items ------------------
+    final action = CupertinoActionSheet(
+      title: Text("Which fruits do you like?"),
+      message: Text("Select the items you like:"),
+      actions: [
+        CupertinoActionSheetAction(onPressed: (){}, child: Text("Mango")),
+        CupertinoActionSheetAction(onPressed: (){}, child: Text("Apple")),
+        CupertinoActionSheetAction(onPressed: (){}, child: Text("Orange")),
+        CupertinoActionSheetAction(onPressed: (){}, child: Text("Lichi")),
+      ],
+      cancelButton: CupertinoActionSheetAction(
+          onPressed: (){Navigator.pop(context);},
+          child: Text("Cancel")
+      ),
+    );
+
+
     return Scaffold(
-      appBar: AppBar(title: Text("Flutter Notebook", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
+      appBar: AppBar(title: Text("Cupertino Action Sheet", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
 
-      // ----------------------- Main Branch  ==> Basic Structure ------------------
+      // ----------------------- Branch 43 ==> CupertinoActionSheet Widget ------------------
 
-      body: Center(),
+      body: Center(
+        child: TextButton(
+            onPressed: (){
+              showCupertinoModalPopup(
+                  context: context,
+                  builder: (context) => action
+              );
+            },
+            child: Text("Show Action Sheet")
+        ),
+      ),
     );
   }
 }

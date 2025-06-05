4c0119b12ef99ef1f52874633233f11dfaf6acc4
Monir <monir.nub.cse996@gmail.com>
2025-06-05 10:59:04 +0600

    auto complete - initial

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -8,14 +8,59 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  // ----------------------- Suggestions, Text Controller ------------------
+
+  List<String> suggestionList = ["Flutter", "Dart", "Kotlin", "Java", "Python"];
+  TextEditingController textEditingController = TextEditingController();
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Flutter Notebook", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
+      appBar: AppBar(title: Text("AutoComplete Widget", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
 
-      // ----------------------- Main Branch  ==> Basic Structure ------------------
+      // ----------------------- Branch 38 ==> AutoComplete Widget ------------------
 
-      body: Center(),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+
+          child: Autocomplete(
+
+              fieldViewBuilder: (context, textEditingController, focusNode, onFieldSubmitted) {
+
+                return TextField(
+                  controller: textEditingController,
+                  decoration: InputDecoration(hintText: "Enter Text"),
+                );
+              },
+
+              optionsBuilder: (value) {
+
+               if(value.text == ''){
+                 return const Iterable<String>.empty();
+
+               } else {
+                 List<String> matches = [];
+                 matches.addAll(suggestionList);
+
+                 matches.retainWhere((s){
+                   return s.toLowerCase().contains(value.text.toLowerCase());
+                 });
+
+                 return matches;
+               }
+              },
+
+              onSelected: (val){
+                print("You hae selected $val");
+              },
+
+
+          ),
+        ),
+      ),
     );
   }
 }

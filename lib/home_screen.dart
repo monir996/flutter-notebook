c1dec409d686933f5a3f91ab33f9d9ca89a71bae
Monir <monir.nub.cse996@gmail.com>
2025-06-05 12:49:32 +0600

    url launcher - initial

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({super.key});
@@ -8,14 +10,64 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  // ----------------------- Exception Handle ------------------
+  Future<void> _launchUrl(Uri url) async {
+    if(!await launchUrl(url)){
+      throw Exception('Could not launch $url');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Flutter Notebook", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
+      appBar: AppBar(title: Text("URL Launcher", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
 
-      // ----------------------- Main Branch  ==> Basic Structure ------------------
+      // ----------------------- Branch 40 ==> URL Launcher Widget ------------------
 
-      body: Center(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            // ----------------------- Launch URL ------------------
+            CupertinoButton.filled(
+                onPressed: (){
+                  final Uri url = Uri.parse('https://google.com/');
+                  _launchUrl(url);
+                },
+                child: Text("Go to google")
+            ),
+
+            SizedBox(height: 20),
+
+            // ----------------------- Call ------------------
+            CupertinoButton.filled(
+                onPressed: (){
+
+                  String number = "+8801096543908";
+                  final Uri url = Uri.parse('tel:$number');
+                  _launchUrl(url);
+                },
+                child: Text("Call us")
+            ),
+
+            SizedBox(height: 20),
+
+            // ----------------------- SMS ------------------
+            CupertinoButton.filled(
+                onPressed: (){
+
+                  String number = "+8801096543908";
+                  final Uri url = Uri.parse('sms:$number');
+                  _launchUrl(url);
+                },
+                child: Text("Send sms")
+            ),
+
+          ],
+        ),
+      ),
     );
   }
 }

349267aaa96c24a9cfd9e6adb1b2b7b4d203aad6
Monir <monir.nub.cse996@gmail.com>
2025-06-05 11:42:08 +0600

    stepper - initial

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -8,14 +8,67 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  // ----------------------- Step List ------------------
+  int _currentActiveStep = 0;
+
+  List<Step> stepList()=> [
+    Step(
+        isActive: _currentActiveStep >= 0,
+        title: Text("Beginner"),
+        content: Text("You are a beginner Programmer.")
+    ),
+    Step(
+        isActive: _currentActiveStep >= 1,
+        title: Text("Mid Level"),
+        content: Text("You are a mid level Programmer.")
+    ),
+    Step(
+        isActive: _currentActiveStep >= 2,
+        title: Text("Pro"),
+        content: Text("You are a pro Programmer.")
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Flutter Notebook", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
+      appBar: AppBar(title: Text("Stepper Widget", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
 
-      // ----------------------- Main Branch  ==> Basic Structure ------------------
+      // ----------------------- Branch 39 ==> Stepper Widget ------------------
 
-      body: Center(),
+      body: Stepper(
+
+          steps: stepList(),
+          //type: StepperType.horizontal,
+          currentStep: _currentActiveStep,
+
+          onStepContinue: (){
+            if(_currentActiveStep < (stepList().length - 1)) {
+             setState(() {
+               _currentActiveStep += 1;
+             });
+            }
+          },
+
+          onStepCancel: (){
+            if(_currentActiveStep == 0 ) {
+              return;
+            }
+
+            setState(() {
+              _currentActiveStep -= 1;
+            });
+          },
+
+          onStepTapped: (index){
+            setState(() {
+              _currentActiveStep = index;
+            });
+          },
+
+      ),
+
     );
   }
 }

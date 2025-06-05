2f909cba7b60bedbaf32bc47986b2e67442ba6b1
Monir <monir.nub.cse996@gmail.com>
2025-06-05 13:26:54 +0600

    modal bottom sheet - initial

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -11,11 +11,58 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Flutter Notebook", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
+      appBar: AppBar(title: Text("Modal Bottom Sheet", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
 
-      // ----------------------- Main Branch  ==> Basic Structure ------------------
+      // ----------------------- Branch 42 ==> ModalBottomSheet Widget------------------
 
-      body: Center(),
+      body: Center(
+        child: TextButton(
+            onPressed: (){
+
+              showModalBottomSheet(
+
+                  //backgroundColor: Colors.tealAccent,
+                  //isScrollControlled: true,
+                  //shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+                  context: context,
+                  builder: (context) {
+
+                    return Padding(
+                      padding: const EdgeInsets.all(10.0),
+
+                      child: Column(
+                        //mainAxisSize: MainAxisSize.min,
+                        children: [
+
+                          ListTile(
+                            title: Text("Copy"),
+                            leading: Icon(Icons.copy),
+                            onTap: (){Navigator.pop(context);},
+                          ),
+
+                          ListTile(
+                            title: Text("Edit"),
+                            leading: Icon(Icons.edit),
+                            onTap: (){Navigator.pop(context);},
+                          ),
+
+                          ListTile(
+                            title: Text("Share"),
+                            leading: Icon(Icons.share),
+                            onTap: (){Navigator.pop(context);},
+                          ),
+
+                        ],
+
+                      ),
+                    );
+                  }
+              );
+            },
+            child: Text("Show Modal Sheet")
+
+        ),
+      ),
     );
   }
 }

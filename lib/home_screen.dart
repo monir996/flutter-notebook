8f03108c17befc6f20cd3c5efff18ddb909e0be3
Monir <monir.nub.cse996@gmail.com>
2025-06-05 15:15:16 +0600

    alert dialog - initial

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -10,12 +10,42 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+
+    // ----------------------- Create Alert Dialog  ------------------
+    myAlertDialog(context, String title, String message) {
+
+      return showDialog(
+          context: context,
+          builder: (BuildContext context) {
+
+            return AlertDialog(
+              title: Text(title),
+              content: Text(message),
+              actions: [
+                TextButton(onPressed: (){Navigator.pop(context);}, child: Text("Yes")),
+
+                TextButton(onPressed: (){Navigator.pop(context);}, child: Text("No")),
+              ],
+
+            );
+          }
+      );
+    }
+
+
     return Scaffold(
-      appBar: AppBar(title: Text("Flutter Notebook", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
+      appBar: AppBar(title: Text("Alert Dialog", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
 
-      // ----------------------- Main Branch  ==> Basic Structure ------------------
+      // ----------------------- Branch 44 ==> AlertDialog Widget ------------------
 
-      body: Center(),
+      body: Center(
+        child: TextButton(
+            onPressed: (){
+              myAlertDialog(context, "Confirmation", "Do you want to delete?");
+            },
+            child: Text("Show Dialog")
+        ),
+      ),
     );
   }
 }

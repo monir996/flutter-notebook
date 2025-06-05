95e854476898d492dacec92a8c56b11d0b235b9c
Monir <monir.nub.cse996@gmail.com>
2025-06-06 04:47:35 +0600

    media query - initial

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -12,12 +12,35 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
 
+    var height = MediaQuery.of(context).size.height / 3;
+    var width = MediaQuery.of(context).size.width / 2;
+
     return Scaffold(
-      appBar: AppBar(title: Text("Flutter Notebook", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
+      appBar: AppBar(title: Text("MediaQuery", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
 
-      // ----------------------- Main Branch  ==> Basic Structure ------------------
+      // ----------------------- Branch 52 ==> MediaQuery ------------------
 
-      body: Center(),
+      // MediaQuery is used to get information about the screen size,
+      // orientation, and other device properties. It helps make the UI
+      // responsive based on different screen sizes and devices.
+
+
+      body: SafeArea(
+
+        child: Container(
+          height: height,
+          width: width,
+          color: Colors.deepOrange,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Width: $width", style: TextStyle(color: Colors.white, fontSize: width * 0.05)),
+              Text("Height: $height", style: TextStyle(color: Colors.white, fontSize: width * 0.05)),
+            ],
+          ),
+        ),
+
+      ),
     );
   }
 }

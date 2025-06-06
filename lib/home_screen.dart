6abb094d036272df61cd59057d626a16eae7dd8f
Monir <monir.nub.cse996@gmail.com>
2025-06-06 20:07:57 +0600

    future builder - initial

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -9,15 +9,42 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
 
+  // ----------------------- Create Future Builder ------------------
+  Future<String> getData() {
+    return Future.delayed(Duration(seconds: 3), (){
+      return "Data loaded";
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
-      appBar: AppBar(title: Text("Flutter Notebook", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
+      appBar: AppBar(title: Text("Future Builder", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
 
-      // ----------------------- Main Branch  ==> Basic Structure ------------------
+      // ----------------------- Branch 56 ==> Future Builder ------------------
 
-      body: Center(),
+      body: Center(
+
+        child: FutureBuilder(
+            future: getData(),
+            builder: (context, snapshot) {
+
+              if(snapshot.connectionState == ConnectionState.waiting) {
+                return CircularProgressIndicator();
+              }
+
+              if(snapshot.hasData) {
+                return Text(snapshot.data.toString());
+              }
+              else {
+                return Text(snapshot.error.toString());
+              }
+            }
+        ),
+
+      ),
     );
   }
 }

081c2264870a0f793c0672095224c0ccd0140c8f
Monir <monir.nub.cse996@gmail.com>
2025-06-06 19:36:29 +0600

    location - initial

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,15 +10,64 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
 
+  // ----------------------- Initialize Location and Get Permissions ------------------
+  Location location = Location();
+  late bool _serviceEnabled;
+  late PermissionStatus _permissionStatus;
+  late LocationData _locationData;
+  late String latitude = "", longitude = "";
+
+  initLocation() async {
+    _serviceEnabled = await location.serviceEnabled();
+    if(!_serviceEnabled){
+      _serviceEnabled = await location.requestService();
+      if(!_serviceEnabled){
+        return;
+      }
+    }
+
+    _permissionStatus = await location.hasPermission();
+    if(_permissionStatus == PermissionStatus.denied){
+      _permissionStatus = await location.requestPermission();
+
+      if(_permissionStatus != PermissionStatus.granted){
+        return;
+      }
+    }
+
+    _locationData = await location.getLocation();
+
+    latitude = _locationData.latitude.toString();
+    longitude = _locationData.longitude.toString();
+
+    location.enableBackgroundMode(enable: true);
+    location.onLocationChanged.listen((LocationData currentLocation){});
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    initLocation();
+  }
+
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
-      appBar: AppBar(title: Text("Flutter Notebook", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
+      appBar: AppBar(title: Text("Location Package", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
 
-      // ----------------------- Main Branch  ==> Basic Structure ------------------
+      // ----------------------- Branch 54 ==> Location Package ------------------
 
-      body: Center(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Latitude: $latitude"),
+            Text("Longitude: $longitude")
+          ],
+        ),
+      ),
     );
   }
 }

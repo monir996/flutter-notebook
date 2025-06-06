bda30ca6c332aec91dabd6f24d47a1491d1b24d8
Monir <monir.nub.cse996@gmail.com>
2025-06-06 19:53:06 +0600

    device info - initial

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -9,15 +10,53 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
 
+  // ----------------------- Initialize Device Info ------------------
+  DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
+  late AndroidDeviceInfo androidDeviceInfo;
+  late IosDeviceInfo iosDeviceInfo;
+  late WebBrowserInfo webBrowserInfo;
+
+  getAndroidDeviceInfo()async {
+    androidDeviceInfo = await deviceInfoPlugin.androidInfo;
+    setState(() {});
+  }
+
+  getIosDeviceInfo()async {
+    iosDeviceInfo = await deviceInfoPlugin.iosInfo;
+    setState(() {});
+  }
+
+  getWebBrowserInfo()async {
+    webBrowserInfo = await deviceInfoPlugin.webBrowserInfo;
+    setState(() {});
+  }
+  
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getAndroidDeviceInfo();
+    getIosDeviceInfo();
+    getWebBrowserInfo();
+  }
+
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
-      appBar: AppBar(title: Text("Flutter Notebook", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
+      appBar: AppBar(title: Text("Device Info", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
 
-      // ----------------------- Main Branch  ==> Basic Structure ------------------
+      // ----------------------- Branch 55 ==> Device Info ------------------
 
-      body: Center(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Android Model: ${androidDeviceInfo.model.toString()}"),
+            Text("Android Manufacturer: ${androidDeviceInfo.manufacturer.toString()}"),
+          ],
+        ),
+      ),
     );
   }
 }

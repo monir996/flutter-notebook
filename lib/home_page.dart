7cf7f27aa5de4df988b28ae784e697b00f798599
monir <monir.nub.cse996@gmail.com>
2025-06-21 21:01:06 +0600

    navigation and pass data - initial

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook/second_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,17 +9,80 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  final TextEditingController _name = TextEditingController();
+  final TextEditingController _email = TextEditingController();
+  final TextEditingController _phone = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          backgroundColor: Colors.blue,
-          foregroundColor: Colors.white,
-          title: Text('Navigation')
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+        title: Text('Navigation'),
+        centerTitle: true,
       ),
 
-        // ----------------------- Branch 63 ==> Navigation and Pass Data ------------------
-      body: Center(),
+      // ----------------------- Branch 63 ==> Navigation and Pass Data ------------------
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: _name,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Enter your Name',
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: _email,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Enter your Email',
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: _phone,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Enter your Phone No',
+                ),
+              ),
+            ),
+
+            SizedBox(height: 20),
+
+            //--------------- Navigate to Second Page
+            MaterialButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        SecondPage(name: _name.text, email: _email.text, phone: _phone.text),
+                  ),
+                );
+              },
+              color: Colors.blue,
+              textColor: Colors.white,
+              child: Text('Go Second Page'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
